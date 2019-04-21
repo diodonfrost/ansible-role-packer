ff2adbb74b75622827010eeeac6cc0216095e623
diodonfrost <diodon.frost@diodonfrost.me>
2019-04-21 16:57:51 +0200

    add FreeBSD support

--- tests/vagrant/vagrant_freebsd_target.rb
+++ tests/vagrant/vagrant_freebsd_target.rb
@@ -1,6 +1,6 @@
 Vagrant.configure(2) do |config|
   config.vm.provision "shell", inline: <<-SHELL
-    pkg install -y ncurses python
+    pkg install -y ncurses python gtar unzip
     pkg install -y py27-ansible
     ln -s /usr/local/bin/python2.7 /usr/bin/python
   SHELL
